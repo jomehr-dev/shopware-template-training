c803b36baafcb6fca22c65bcb0857a10b7ba4bb7
jomehr <jonathan.mehrwald@gmail.com>
2022-07-17 22:45:52 +0200

    customized default product listing and theme default colors

---
+++ src/Resources/app/storefront/dist/storefront/js/template-training.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["template-training"],{
+
+/***/ "Jidp":
+/*!****************************************************************************************************************************************!*\
+  !*** /Users/jomehr/Sites/private/how-to-shopware/development/custom/plugins/TemplateTraining/src/Resources/app/storefront/src/main.js ***!
+  \****************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("console.info(\"test\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiSmlkcC5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy8vVXNlcnMvam9tZWhyL1NpdGVzL3ByaXZhdGUvaG93LXRvLXNob3B3YXJlL2RldmVsb3BtZW50L2N1c3RvbS9wbHVnaW5zL1RlbXBsYXRlVHJhaW5pbmcvc3JjL1Jlc291cmNlcy9hcHAvc3RvcmVmcm9udC9zcmMvbWFpbi5qcz9kOWY4Il0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUuaW5mbyhcInRlc3RcIik7XG4iXSwibWFwcGluZ3MiOiJBQUFBIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///Jidp\n");
+
+/***/ })
+
+},[["Jidp","runtime"]]]);
